3ea2c1cc930e3e72b0159d40fbb5d80bcc697c38
Paulo Camacho <paulo.ccamacho@gmail.com>
2020-02-27 14:14:15 -0800

    changed format of app.js

---
+++ google-book-search/src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props)=> (
+    <h1 className="lead">
+        <b>
+            Google Book Search
+        </b>
+    </h1>
+);
+
+export default Header;
